4a5cc7aca183184abe13b89d59103386134a9851
NguyenNguyenNguyenPhong <109236721+NguyenNguyenNguyenPhong@users.noreply.github.com>
2024-01-04 22:44:45 +0700

    ++ Commit api to test send kafka

--- demo/src/main/java/com/example/demo/controller/LogController.java
+++ demo/src/main/java/com/example/demo/controller/LogController.java
@@ -1,28 +1,28 @@
-package com.example.demo.controller;// LogController.java
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-@RequestMapping("/api/logs")
-public class LogController {
-
-    private final KafkaTemplate<String, String> kafkaTemplate;
-
-    @Autowired
-    public LogController(KafkaTemplate<String, String> kafkaTemplate) {
-        this.kafkaTemplate = kafkaTemplate;
-    }
-
-    @PostMapping
-    public String postLog(@RequestBody String logMessage) {
-        // Gửi log message lên Kafka
-        kafkaTemplate.send("logs", logMessage);
-
-        // Trả về thông báo thành công
-        return "Log sent to Kafka: " + logMessage;
-    }
-}
+//package com.example.demo.controller;// LogController.java
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.kafka.core.KafkaTemplate;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//@RestController
+//@RequestMapping("/api/logs")
+//public class LogController {
+//
+//    private final KafkaTemplate<String, String> kafkaTemplate;
+//
+//    @Autowired
+//    public LogController(KafkaTemplate<String, String> kafkaTemplate) {
+//        this.kafkaTemplate = kafkaTemplate;
+//    }
+//
+//    @PostMapping
+//    public String postLog(@RequestBody String logMessage) {
+//        // Gửi log message lên Kafka
+//        kafkaTemplate.send("logs", logMessage);
+//
+//        // Trả về thông báo thành công
+//        return "Log sent to Kafka: " + logMessage;
+//    }
+//}
